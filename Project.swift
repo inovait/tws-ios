6b95d043b9bceff0d5a564322ce7e2f8b60dafa3
Miha Hozjan <miha.hozjan@inova.si>
2024-05-22 10:40:41 +0200

    Updated tuist

--- Project.swift
+++ Project.swift
@@ -1,38 +1,6 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let deploymentTarget = "17.0"
-
-// Plist
-
-let infoPlist: [String: Plist.Value] = [
-    "UILaunchScreen": [:],
-    "CFBundleDisplayName": "The Web Snippet",
-    "CFBundleShortVersionString": "$(MARKETING_VERSION)",
-    "CFBundleVersion": "${CURRENT_PROJECT_VERSION}"
-]
-
-// Scripts
-
-let targetScripts: [TargetScript] = [
-    .post(
-        script: #"""
-        "${BUILD_DIR%/Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
-        """#,
-        name: "Firebase Crashlystics",
-        inputPaths: [
-            "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
-            "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
-            "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist",
-            "$(TARGET_BUILD_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
-            "$(TARGET_BUILD_DIR)/$(EXECUTABLE_PATH)"
-        ],
-        basedOnDependencyAnalysis: false
-    ),
-]
-
-// Project
-
 let project = Project(
     name: "TheWebSnippet",
     organizationName: "Inova IT, d.o.o.",
@@ -49,11 +17,11 @@ let project = Project(
             destinations: .iOS,
             product: .app,
             bundleId: "com.inova.tws",
-            deploymentTargets: .iOS(deploymentTarget),
-            infoPlist: .extendingDefault(with: infoPlist),
+            deploymentTargets: .iOS(deploymentTarget()),
+            infoPlist: .extendingDefault(with: infoPlist()),
             sources: ["TWSDemo/Sources/**"],
             resources: ["TWSDemo/Resources/**"],
-            scripts: targetScripts,
+            scripts: targetScripts(),
             dependencies: [
                 .external(name: "FirebaseAnalytics"),
                 .external(name: "FirebaseCrashlytics")
@@ -75,7 +43,7 @@ let project = Project(
             destinations: .iOS,
             product: .unitTests,
             bundleId: "com.inova.twsTests",
-            deploymentTargets: .iOS(deploymentTarget),
+            deploymentTargets: .iOS(deploymentTarget()),
             infoPlist: .default,
             sources: ["TWSDemoTests/Sources/**"],
             dependencies: [
@@ -94,3 +62,35 @@ let project = Project(
         )
     ]
 )
+
+func deploymentTarget() -> String {
+    "17.0"
+}
+
+func infoPlist() -> [String: Plist.Value] {
+    [
+        "UILaunchScreen": [:],
+        "CFBundleDisplayName": "The Web Snippet",
+        "CFBundleShortVersionString": "$(MARKETING_VERSION)",
+        "CFBundleVersion": "${CURRENT_PROJECT_VERSION}"
+    ]
+}
+
+func targetScripts() -> [TargetScript] {
+    [
+        .post(
+            script: #"""
+            "${BUILD_DIR%/Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/run"
+            """#,
+            name: "Firebase Crashlystics",
+            inputPaths: [
+                "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
+                "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
+                "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Info.plist",
+                "$(TARGET_BUILD_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/GoogleService-Info.plist",
+                "$(TARGET_BUILD_DIR)/$(EXECUTABLE_PATH)"
+            ],
+            basedOnDependencyAnalysis: false
+        ),
+    ]
+}
