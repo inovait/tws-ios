650af05bbee2f5d34d4aec3daa64aff29d209853
Luka Kit <luka.kit@inova.si>
2024-06-05 13:47:09 +0200

    [WEB-31]: Fixed import warning for TWSLogger in TWSKit

--- TWSKit/Sources/Logger.swift
+++ TWSKit/Sources/Logger.swift
@@ -5,6 +5,6 @@
 //  Created by Luka Kit on 3. 6. 24.
 //  Copyright © 2024 Inova IT, d.o.o. All rights reserved.
 //
-import TWSLogger
+@_implementationOnly import TWSLogger
 
 let logger = TWSLog(category: "TWSKit")
