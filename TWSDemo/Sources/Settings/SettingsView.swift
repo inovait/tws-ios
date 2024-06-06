920510c83a869e4db3c3f8851334ce473474431e
Miha Hozjan <miha.hozjan@inova.si>
2024-06-06 11:08:44 +0200

    WEB-34: feat: let the user switch from API request to custom URLs

--- TWSDemo/Sources/Settings/SettingsView.swift
+++ TWSDemo/Sources/Settings/SettingsView.swift
@@ -11,18 +11,67 @@ import TWSKit
 
 struct SettingsView: View {
 
+    @State private var selection: Selection = .apiResponse
+    @State private var localURLs: String = ""
+
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("v\(_appVersion())")
+            Form {
+                Section(header: Text("Source Selection")) {
+                    VStack {
+                        Picker("Source", selection: $selection) {
+                            ForEach(Selection.allCases, id: \.self) { selection in
+                                Text(selection.rawValue).tag(selection)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        if selection == .localURLs {
+                            TextField("Custom URLs", text: $localURLs, axis: .vertical)
+                                .toolbar {
+                                    ToolbarItemGroup(placement: .keyboard) {
+                                        HStack {
+                                            Spacer()
+                                            
+                                            Button("Done") {
+                                                UIApplication.shared.endEditing()
+                                            }
+                                        }
+                                    }
+                                }
+                                .padding(4)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                )
+                                .padding(.vertical, 2)
+                        }
+                    }
+                }
+
+                Section(header: Text("About")) {
+                    Text("v\(_appVersion())")
+                }
             }
-            .navigationTitle("Settings")
+            .scrollDismissesKeyboard(.immediately)
         }
+        .navigationTitle("Settings")
     }
+}
+
+enum Selection: String, CaseIterable {
+    case apiResponse = "API Response"
+    case localURLs = "Local URLs"
 }
 
 private func _appVersion() -> String {
     let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
     let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
     return "\(version) (\(build))"
+}
+
+private extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
--- TWSDemo/Sources/SnippetsTab/SnippetsTabView.swift
+++ TWSDemo/Sources/SnippetsTab/SnippetsTabView.swift
@@ -47,25 +47,27 @@ struct SnippetsTabView: View {
 
     @ViewBuilder
     private func _selectionView() -> some View {
-        HStack(spacing: 1) {
-            ForEach(Array(zip(twsViewModel.snippets.indices, twsViewModel.snippets)), id: \.1.id) { idx, item in
-                Button {
-                    withAnimation {
-                        selectedId = item.id
+        if twsViewModel.snippets.count > 1 {
+            HStack(spacing: 1) {
+                ForEach(Array(zip(twsViewModel.snippets.indices, twsViewModel.snippets)), id: \.1.id) { idx, item in
+                    Button {
+                        withAnimation {
+                            selectedId = item.id
+                        }
+                    } label: {
+                        VStack {
+                            Text("\(idx + 1)")
+                                .font(.title)
+                                .foregroundColor(selectedId == item.id ? Color.accentColor : Color.gray)
+                            
+                            Rectangle()
+                                .fill(Color.accentColor)
+                                .frame(height: selectedId == item.id ? 1 : 0)
+                                .frame(maxWidth: .infinity)
+                                .padding(.bottom, 1)
+                        }
+                        .frame(minWidth: 75, maxWidth: .infinity)
                     }
-                } label: {
-                    VStack {
-                        Text("\(idx + 1)")
-                            .font(.title)
-                            .foregroundColor(selectedId == item.id ? Color.accentColor : Color.gray)
-
-                        Rectangle()
-                            .fill(Color.accentColor)
-                            .frame(height: selectedId == item.id ? 1 : 0)
-                            .frame(maxWidth: .infinity)
-                            .padding(.bottom, 1)
-                    }
-                    .frame(minWidth: 75, maxWidth: .infinity)
                 }
             }
         }
