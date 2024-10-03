cf62d425088be280b1f0f9d0024c6d55c2440298
Luka Kit <luka.kit@inova.si>
2024-10-03 12:26:58 +0200

    Removed empty spaces

--- TWSSnippetsTests/Sources/SnippetsTests.swift
+++ TWSSnippetsTests/Sources/SnippetsTests.swift
@@ -28,8 +28,7 @@ final class SnippetsTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
-    
+
     @MainActor
     func testAPIShouldNotOverrideState() async {
         let s1ID = UUID()
