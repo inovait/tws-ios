7404ec787d65f827802f25d396c7a9eb2e28bb0b
Miha Hozjan <miha.hozjan@inova.si>
2024-10-15 13:37:29 +0200

    Fixed unit tests

--- TWSLoggerTests/Sources/TWSLoggerTests.swift
+++ TWSLoggerTests/Sources/TWSLoggerTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import OSLog
+import ComposableArchitecture
 @testable import TWSLogger
 
 final class TWSLoggerTests: XCTestCase {
@@ -32,11 +33,11 @@ final class TWSLoggerTests: XCTestCase {
         logManager.warn("This is a warning", functionName: "Function 1")
         logManager.info("This is an info")
 
-        let exepectedLogResult =
-            "This is an error [Class 1, l34]\n" +
+        let expectedLogResult =
+            "This is an error [Class 1, l31]\n" +
             "This is a message [TWSLoggerTests/TWSLoggerTests.swift, l13]\n" +
-            "This is a warning [TWSLoggerTests/TWSLoggerTests.swift, l36]\n" +
-            "This is an info [TWSLoggerTests/TWSLoggerTests.swift, l37]\n"
+            "This is a warning [TWSLoggerTests/TWSLoggerTests.swift, l33]\n" +
+            "This is an info [TWSLoggerTests/TWSLoggerTests.swift, l34]\n"
 
         // Fetching the logs and checking if they're present in the log report
         do {
@@ -59,7 +60,12 @@ final class TWSLoggerTests: XCTestCase {
             if let fileURL = fileURL {
                 do {
                     let fileContent = try String(contentsOf: fileURL)
-                    XCTAssertEqual(fileContent, exepectedLogResult)
+
+                    XCTAssertEqual(
+                        fileContent,
+                        expectedLogResult,
+                        diff(fileContent.description, expectedLogResult.description) ?? ""
+                    )
                 } catch {
                     XCTFail("Failed to read file content: \(error)")
                 }
