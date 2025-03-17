5fe85860ddb8aa78dd4299f80a913a276ae49511
SvenKotnik <sven.kotnik@inova.si>
2025-03-17 08:51:40 +0100

    refactor: change the order and name of articles

---
+++ Sources/TWS/Sources/Documentation.docc/Tutorials/02_RemoteSnippet/RemoteSnippet_05.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import TWS
+
+@main
+struct TWSDemoApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .twsEnable(configuration: TWSBasicConfiguration(
+                    id: "<PROJECT_ID>"
+                ))
+        }
+    }
+}
+
+struct HomeView: View {
+
+    @Environment(TWSManager.self) var tws
+
+    var  body: some View {
+        TabView {
+            ForEach(tws.snippets()) { snippet in
+
+            }
+        }
+    }
+}
