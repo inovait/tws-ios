294f730decc4a0cb40f5bc94971b2df0c7acd213
SvenKotnik <sven.kotnik@inova.si>
2025-05-12 13:07:12 +0200

    feat: let user set a flag to enable/disable pull to refresh

--- Sources/TWS/Sources/Views/WebView/TWSView.swift
+++ Sources/TWS/Sources/Views/WebView/TWSView.swift
@@ -38,6 +38,7 @@ public struct TWSView: View {
     let cssOverrides: [TWSRawCSS]
     let jsOverrides: [TWSRawJS]
     let overrideVisibilty: Bool
+    let enablePullToRefresh: Bool
 
     /// Main contructor
     /// - Parameters:
@@ -50,13 +51,15 @@ public struct TWSView: View {
         state: Bindable<TWSViewState> = .init(.init(loadingState: .loaded)),
         cssOverrides: [TWSRawCSS] = [],
         jsOverrides: [TWSRawJS] = [],
-        overrideVisibilty: Bool = false
+        overrideVisibilty: Bool = false,
+        enablePullToRefresh: Bool = false
     ) {
         self.snippet = snippet
         self.cssOverrides = cssOverrides
         self.jsOverrides = jsOverrides
         self.overrideVisibilty = overrideVisibilty
         self._state = state
+        self.enablePullToRefresh = enablePullToRefresh
     }
 
     public var body: some View {
@@ -76,7 +79,8 @@ public struct TWSView: View {
                             cssOverrides: cssOverrides,
                             jsOverrides: jsOverrides,
                             displayID: displayID,
-                            state: $state
+                            state: $state,
+                            enablePullToRefresh: enablePullToRefresh
                         )
                         .id(snippet.id)
                         // The actual URL changed for the same Snippet ~ redraw is required
@@ -137,19 +141,22 @@ private struct _TWSView: View {
     let cssOverrides: [TWSRawCSS]
     let jsOverrides: [TWSRawJS]
     let displayID: String
-
+    let enablePullToRefresh: Bool
+    
     init(
         snippet: TWSSnippet,
         cssOverrides: [TWSRawCSS],
         jsOverrides: [TWSRawJS],
         displayID id: String,
-        state: Bindable<TWSViewState>
+        state: Bindable<TWSViewState>,
+        enablePullToRefresh: Bool
     ) {
         self.snippet = snippet
         self.cssOverrides = cssOverrides
         self.jsOverrides = jsOverrides
         self.displayID = id.trimmingCharacters(in: .whitespacesAndNewlines)
         self._state = state
+        self.enablePullToRefresh = enablePullToRefresh
     }
 
     var body: some View {
@@ -174,7 +181,8 @@ private struct _TWSView: View {
             canGoBack: $navigator.canGoBack,
             canGoForward: $navigator.canGoForward,
             downloadCompleted: onDownloadCompleted,
-            state: $state
+            state: $state,
+            enablePullToRefresh: enablePullToRefresh
         )
         // Used for Authentication via Safari
         .onOpenURL { url in openURL = url }
