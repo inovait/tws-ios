67a3fb90e9e46151b2911871d92ed60226cf8fc2
SvenKotnik <sven.kotnik@inova.si>
2025-05-21 14:29:19 +0200

    fix: call all delegate methods with coordinator implementation as well

--- Sources/TWS/Sources/Views/WebView/TWSView+WebViewWithErrorOverlay.swift
+++ Sources/TWS/Sources/Views/WebView/TWSView+WebViewWithErrorOverlay.swift
@@ -138,16 +138,82 @@ class PopupNavigationDelegate: NSObject, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         onEndNavigation()
+        guard let navigation else { return }
         coordinator?.webView?(webView, didFinish: navigation)
     }
 
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         onEndNavigation()
+        guard let navigation else { return }
         coordinator?.webView?(webView, didFail: navigation, withError: error)
     }
 
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         onEndNavigation()
+        guard let navigation else { return }
         coordinator?.webView?(webView, didFailProvisionalNavigation: navigation, withError: error)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        didStartProvisionalNavigation navigation: WKNavigation!
+    ) {
+        guard let navigation else { return }
+        coordinator?.webView?(webView, didStartProvisionalNavigation: navigation)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!
+    ) {
+        guard let navigation else { return }
+        coordinator?.webView?(webView, didReceiveServerRedirectForProvisionalNavigation: navigation)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        didCommit navigation: WKNavigation!
+    ) {
+        guard let navigation else { return }
+        coordinator?.webView?(webView, didCommit: navigation)
+    }
+
+    func webViewWebContentProcessDidTerminate(
+        _ webView: WKWebView
+    ) {
+        coordinator?.webViewWebContentProcessDidTerminate?(webView)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        preferences: WKWebpagePreferences,
+        decisionHandler: @escaping @MainActor @Sendable (WKNavigationActionPolicy, WKWebpagePreferences) -> Void
+    ) {
+        coordinator?.webView?(webView, decidePolicyFor: navigationAction, preferences: preferences, decisionHandler: decisionHandler)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationResponse: WKNavigationResponse,
+        decisionHandler: @escaping @MainActor @Sendable (WKNavigationResponsePolicy) -> Void
+    ) {
+        coordinator?.webView?(webView, decidePolicyFor: navigationResponse, decisionHandler: decisionHandler)
+    }
+
+    func webView(
+        _ webView: WKWebView,
+        didReceive challenge: URLAuthenticationChallenge,
+        completionHandler: @escaping @MainActor @Sendable (URLSession.AuthChallengeDisposition, URLCredential?) -> Void
+    ) {
+        coordinator?.webView?(webView, didReceive: challenge, completionHandler: completionHandler)
+    }
+
+    func webView(_ webView: WKWebView, navigationResponse: WKNavigationResponse, didBecome download: WKDownload) {
+        coordinator?.webView?(webView, navigationResponse: navigationResponse, didBecome: download)
+    }
+
+    func webView(_ webView: WKWebView, navigationAction: WKNavigationAction, didBecome download: WKDownload) {
+        coordinator?.webView?(webView, navigationAction: navigationAction, didBecome: download)
     }
 }
