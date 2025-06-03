f33f8e2b7f036b33cf0181e8717069505729f6e2
SvenKotnik <sven.kotnik@inova.si>
2025-06-03 14:27:35 +0200

    refactor: implemented new way of setting and registering manager in sample app

--- Examples/Sample/Sample/ContentView.swift
+++ Examples/Sample/Sample/ContentView.swift
@@ -48,6 +48,7 @@ struct ContentView: View {
                     }
             }
         }
+        .twsRegisterManager(manager: tws)
     }
 }
 
--- Examples/Sample/Sample/SampleApp.swift
+++ Examples/Sample/Sample/SampleApp.swift
@@ -7,7 +7,7 @@ struct SampleApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .twsEnable(configuration: TWSBasicConfiguration(id: "<TWS_PROJECT>"))
+                .twsSetManager(configuration: TWSBasicConfiguration(id: "<TWS_PROJECT>"))
                 .onAppear {
                     UIPageControl.appearance().currentPageIndicatorTintColor = .red
                     UIPageControl.appearance().pageIndicatorTintColor = UIColor.red.withAlphaComponent(0.2)
